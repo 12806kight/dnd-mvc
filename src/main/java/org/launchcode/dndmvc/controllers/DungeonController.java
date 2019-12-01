a2e3f35fa63cf4a0c8ea92034982bdd429456bd3
Brewer Kight <bkight@atlassian.com>
2019-11-30 22:11:58 -0600

    This is an updated version with Races established.

--- src/main/java/org/launchcode/dndmvc/controllers/DungeonController.java
+++ src/main/java/org/launchcode/dndmvc/controllers/DungeonController.java
@@ -1,25 +1,31 @@
 package org.launchcode.dndmvc.controllers;
 
+import org.launchcode.dndmvc.models.Classes;
 import org.launchcode.dndmvc.models.Dungeon;
+import org.launchcode.dndmvc.models.Races;
+import org.launchcode.dndmvc.models.data.ClassesDao;
 import org.launchcode.dndmvc.models.data.DungeonRepository;
+import org.launchcode.dndmvc.models.data.RacesDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/dungeons/")
-
-
 public class DungeonController {
 
     private final DungeonRepository dungeonRepository;
+
+    @Autowired
+    private ClassesDao classesDao;
+
+    @Autowired
+    private RacesDao racesDao;
+
 
     @Autowired
     public DungeonController(DungeonRepository dungeonRepository) {
@@ -27,8 +33,12 @@ public class DungeonController {
     }
 
     @GetMapping("create")
-    public String showCharacterForm(Dungeon dungeon) {
-        return "add";
+    public String showSignUpForm(Model model) {
+        model.addAttribute("title","Add Character");
+        model.addAttribute(new Dungeon());
+        model.addAttribute("classes", classesDao.findAll());
+        model.addAttribute("races", racesDao.findAll());
+        return "dungeons/add";
     }
 
     @GetMapping("list")
@@ -38,11 +48,18 @@ public class DungeonController {
     }
 
     @PostMapping("add")
-    public String addCharacter(@Valid Dungeon dungeon, BindingResult result, Model model) {
+    public String addStudent(@Valid Dungeon dungeon, BindingResult result, @RequestParam long classesId,@RequestParam long racesId, Model model) {
         if (result.hasErrors()) {
-            return "add";
+            model.addAttribute("title","Add Character");
+            return "dungeons/add";
         }
 
+        Classes classes = classesDao.findById(classesId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + classesId));
+        dungeon.setClasses(classes);
+        Races races = racesDao.findById(racesId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + racesId));
+        dungeon.setRaces(races);
         dungeonRepository.save(dungeon);
         return "redirect:list";
     }
@@ -50,19 +67,27 @@ public class DungeonController {
     @GetMapping("edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
         Dungeon dungeon = dungeonRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid character Id:" + id));
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + id));
         model.addAttribute("dungeon", dungeon);
-        return "update-student";
+        model.addAttribute("classes", classesDao.findAll());
+        model.addAttribute("races", racesDao.findAll());
+        return "dungeons/update";
     }
 
     @PostMapping("update/{id}")
-    public String updateStudent(@PathVariable("id") long id, @Valid Dungeon dungeon, BindingResult result,
+    public String updateStudent(@PathVariable("id") long id, @Valid Dungeon dungeon, BindingResult result,  @RequestParam long classesId,@RequestParam long racesId,
                                 Model model) {
         if (result.hasErrors()) {
             dungeon.setId(id);
-            return "update-student";
+            return "dungeons/update";
         }
 
+        Classes classes = classesDao.findById(classesId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + classesId));
+        dungeon.setClasses(classes);
+        Races races = racesDao.findById(racesId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + racesId));
+        dungeon.setRaces(races);
         dungeonRepository.save(dungeon);
         model.addAttribute("dungeons", dungeonRepository.findAll());
         return "index";
@@ -71,7 +96,7 @@ public class DungeonController {
     @GetMapping("delete/{id}")
     public String deleteStudent(@PathVariable("id") long id, Model model) {
         Dungeon dungeon = dungeonRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dungeon Id:" + id));
         dungeonRepository.delete(dungeon);
         model.addAttribute("dungeons", dungeonRepository.findAll());
         return "index";
