0283a6d28c31b18e10fe6643086efb29f66517a4
Brewer Kight <bkight@atlassian.com>
2019-12-04 18:00:27 -0600

    This is an updated version with User model and User Controller

--- src/main/java/org/launchcode/dndmvc/models/data/UserDao.java
+++ src/main/java/org/launchcode/dndmvc/models/data/UserDao.java
@@ -1,12 +1,10 @@
 package org.launchcode.dndmvc.models.data;
 
-import org.launchcode.dndmvc.models.Dnd;
+import org.launchcode.dndmvc.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
 @Repository
-@Transactional
-public interface DndDao extends CrudRepository<Dnd, Integer> {
+public interface UserDao extends CrudRepository<User, Long> {
+
 }
